37d235bbcf0128d9b5e6f37db5fefc17b7db4bfa
daveckman <davideckman@outlook.com>
2020-06-02 15:31:50 -0500

    speedup formulation of M(x0) for Lipschitz projection
    Replaced for loops with sparse construction of A for Lipschitz projection case. Replaced for loops for constructing b.

--- Source Code/setup_lipschitz_proj.m
+++ Source Code/setup_lipschitz_proj.m
@@ -15,33 +15,33 @@ gamma = prop_params;
 
 k = size(exp_set, 1);
 
-num_constraints = k^2-k;
-A = spalloc(num_constraints, k, 4*k^2);
-C = double.empty(num_constraints, 0);
-b = zeros(num_constraints, 1);
-
 % Construct lhs matrices and rhs vector for Lipschitz
-%%% LOOK INTO FASTER SPARSE CONSTRUCTION
 
-% First set of constraints: 
+% Constraints: 
 % m_i - m_j <= gamma*min{||x_i - x_j||, ||x_i - x_0||} for all i, j = 1, ..., k with i ~= j
 
-for i = 1:k
-    for j = 1:k
-        if i ~= j            
-            if j < i
-                row = (i-1)*(k-1) + j;
-            elseif j > i
-                row = (i-1)*(k-1) + j - 1;
-            end
-            
-            A(row, i) = 1;
-            A(row, j) = -1;
-            b(row) = gamma*min(norm(exp_set(i,:) - exp_set(j,:),2), norm(exp_set(i,:) - x0,2));
+num_constraints = k^2-k;
 
-        end
-    end
-end
+% Construct A
+A_row = [(1:num_constraints)'; (1:num_constraints)'];
+temp = repmat((1:k)', k, 1);
+temp(1:k+1:end) = 0;
+temp = temp(temp > 0);
+A_col = [repelem((1:k)', k-1, 1); temp];
+A_val = [ones(num_constraints,1); -ones(num_constraints,1)];
+A = sparse(A_row, A_col, A_val, num_constraints, k);
+
+% Construct b
+temp1 = repelem(exp_set, k, 1) - repmat(exp_set, k, 1);
+temp1 = sqrt(sum(temp1.^2,2));
+temp2 = repelem(exp_set, k, 1) - repmat(x0, k^2, 1);
+temp2 = sqrt(sum(temp2.^2,2));
+temp = gamma*min(temp1, temp2);
+temp(1:k+1:end) = -1;
+b = temp(temp >= 0);
+
+% Construct c
+C = double.empty(num_constraints, 0);
 
 end
 
