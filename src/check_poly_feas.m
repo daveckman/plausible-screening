1d7a9e019629c1cce5cce4632700258d22275933
daveckman <david.eckman@northwestern.edu>
2020-09-25 14:30:40 -0500

    change LP solved by RPS and rerun timings
    Updated LP solved for RPS. Reran timings on tandem problem.

--- src/check_poly_feas.m
+++ src/check_poly_feas.m
@@ -52,32 +52,51 @@ if isempty(C) % if explicit form of M(x0) is known, skip the optimization
         z = -Inf;
     end
     
-else % Formulate and solve a linear program (via Farkas' lemma)
+else % Formulate and solve a linear program 
     
-    f_LP = bprime - A*sample_mean;
-    A_LP = -speye(p);
-    b_LP = zeros(p,1);
-    Aeq_LP = C';
-    beq_LP = zeros(q,1);
+    % extra variable approach    
+    % Solve z = max_{w, rho} rho s.t. Cw + rho*1_p <= b' - A*muhat
+    % flip objective because of maximization
+    f_LP = [zeros(q,1);-1];
+    A_LP = [C, ones(p, 1)];
+    b_LP = bprime - A*sample_mean;
 
-    switch LP_solver_string
-        case 'MATLAB'
-            options = optimoptions('linprog','Display','none');
-            [~, z, exit_flag] = linprog(f_LP, A_LP, b_LP, Aeq_LP, beq_LP, [], [], [], options);
-            
-            %Check if linear program was unbounded.
-            if exit_flag == -3
-                z = -Inf;
-            end
-            
-        case 'glpk'
-            [~, z, exit_flag] = glpk(f_LP, [A_LP; Aeq_LP], [b_LP; beq_LP], -Inf*ones(size(A_LP,2),1), Inf*ones(size(A_LP,2),1), [repmat('U',size(A_LP,1),1); repmat('S',size(Aeq_LP,1),1)], repmat('C',size(A_LP,2),1), 1, struct('savefilename','SimpleLP'));
-            
-            %Check if linear program was unbounded.
-            if exit_flag == 6 || exit_flag == 111
-                z = -Inf;
-            end
+    options = optimoptions('linprog','Display','none');
+    [~, z, exit_flag] = linprog(f_LP, A_LP, b_LP,[], [], [], [], [], options);
+        
+    %Check if linear program was unbounded.
+    if exit_flag == -3
+        z = -Inf;
     end
+    
+    % Flip sign back
+    z = -z;
+    
+% Farkas' Lemma approach
+%     f_LP = bprime - A*sample_mean;
+%     A_LP = -speye(p);
+%     b_LP = zeros(p,1);
+%     Aeq_LP = C';
+%     beq_LP = zeros(q,1);
+% 
+%     switch LP_solver_string
+%         case 'MATLAB'
+%             options = optimoptions('linprog','Display','none');
+%             [~, z, exit_flag] = linprog(f_LP, A_LP, b_LP, Aeq_LP, beq_LP, [], [], [], options);
+%             
+%             %Check if linear program was unbounded.
+%             if exit_flag == -3
+%                 z = -Inf;
+%             end
+%             
+%         case 'glpk'
+%             [~, z, exit_flag] = glpk(f_LP, [A_LP; Aeq_LP], [b_LP; beq_LP], -Inf*ones(size(A_LP,2),1), Inf*ones(size(A_LP,2),1), [repmat('U',size(A_LP,1),1); repmat('S',size(Aeq_LP,1),1)], repmat('C',size(A_LP,2),1), 1, struct('savefilename','SimpleLP'));
+%             
+%             %Check if linear program was unbounded.
+%             if exit_flag == 6 || exit_flag == 111
+%                 z = -Inf;
+%             end
+%     end
     
 end
     
