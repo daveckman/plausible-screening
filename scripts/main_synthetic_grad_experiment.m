da88f2231e972d0370f0bfd1bfeaa2301f703852
daveckman <david.eckman@northwestern.edu>
2021-03-19 11:13:48 -0500

    test RGPS with further relaxation
    Relaxed M(x0) by adding inequalities (do cancel out objective function values) and then doing RGPS. Tested on synthetic example.

--- scripts/main_synthetic_grad_experiment.m
+++ scripts/main_synthetic_grad_experiment.m
@@ -21,7 +21,7 @@ card_feas_region = size(feas_region, 1);
 
 %% RUN MACROREPLICATIONS
 
-M = 10; % Number of macroreplications
+M = 100; % Number of macroreplications
 
 % Calculate cutoffs for PS
 D_cutoff_d2 = calc_cutoff(k, n_vec, alpha, 'ell2');
@@ -38,6 +38,8 @@ S_indicators_grad = zeros(card_feas_region, M);
 D_x0s_grad = zeros(card_feas_region, M);
 S_poly_indicators_grad = zeros(card_feas_region, M);
 
+Sonlygrad_d2_indicators = zeros(card_feas_region, M);
+Sonlygrad_dinf_indicators = zeros(card_feas_region, M);
 
 print_problem_header(problem_string, feas_region, exp_set, fn_props)
 
@@ -53,28 +55,34 @@ parfor m = 1:M
 
     %[sample_mean, sample_var] = generate_data(m, oracle_string, oracle_n_rngs, exp_set, n_vec, 'ell1');
 
-    % Original Plausible Screening
-    discrep_string = 'ell2';
-    fprintf('Screening solutions for %s discrepancy...\n', discrep_string)
-    [S_indicators_d2(:,m), D_x0s(:,m), S_poly_indicators_d2(:,m), ~] = PO_screen(feas_region, exp_set, sample_mean, sample_var, n_vec, 'ell2', D_cutoff_d2, fn_props, prop_params, LP_solver_string);
-    fprintf('\nResults of PS screening\n-------------------------------------------------------\n')
-    fprintf('\tstandardized discrepancy: \t\t\t\t%s\n', discrep_string)
-    fprintf('\t# of solutions in PS subset: \t\t\t%d\n', sum(S_indicators_d2(:,m)))  
-    fprintf('\t# of solutions in PS relaxed subset: \t%d\n\n', sum(S_poly_indicators_d2(:,m)))
-
-    % Gradient Plausible Screening w/ Dgrad
-    [S_indicators_grad(:,m), D_x0s_grad(:,m), S_poly_indicators_grad(:,m)] = GPS_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_grad);
-    fprintf('\nResults of GPS screening\n-------------------------------------------------------\n')
-    fprintf('\tstandardized discrepancy: \t\t\t\t Gradients\n')
-    fprintf('\t# of solutions in GPS subset: \t\t\t%d\n', sum(S_indicators_grad(:,m)))  
-    fprintf('\t# of solutions in GPS relaxed subset: \t%d\n\n', sum(S_poly_indicators_grad(:,m)))
-
-    % Gradient Plausible Screening w/ Dgradinf
-    [S_indicators_gradinf(:,m), D_x0s_gradinf(:,m), S_poly_indicators_gradinf(:,m)] = GPSinf_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_gradinf);
-    fprintf('\nResults of GPS screening (dinf) \n-------------------------------------------------------\n')
-    fprintf('\tstandardized discrepancy: \t\t\t\t Gradients\n')
-    fprintf('\t# of solutions in GPS inf subset: \t\t\t%d\n', sum(S_indicators_gradinf(:,m)))  
-    fprintf('\t# of solutions in GPS inf relaxed subset: \t%d\n\n', sum(S_poly_indicators_gradinf(:,m)))
+%     % Original Plausible Screening
+%     discrep_string = 'ell2';
+%     fprintf('Screening solutions for %s discrepancy...\n', discrep_string)
+%     [S_indicators_d2(:,m), D_x0s(:,m), S_poly_indicators_d2(:,m), ~] = PO_screen(feas_region, exp_set, sample_mean, sample_var, n_vec, 'ell2', D_cutoff_d2, fn_props, prop_params, LP_solver_string);
+%     fprintf('\nResults of PS screening\n-------------------------------------------------------\n')
+%     fprintf('\tstandardized discrepancy: \t\t\t\t%s\n', discrep_string)
+%     fprintf('\t# of solutions in PS subset: \t\t\t%d\n', sum(S_indicators_d2(:,m)))  
+%     fprintf('\t# of solutions in PS relaxed subset: \t%d\n\n', sum(S_poly_indicators_d2(:,m)))
+% 
+%     % Gradient Plausible Screening w/ Dgrad
+%     [S_indicators_grad(:,m), D_x0s_grad(:,m), S_poly_indicators_grad(:,m)] = GPS_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_grad);
+%     fprintf('\nResults of GPS screening\n-------------------------------------------------------\n')
+%     fprintf('\tstandardized discrepancy: \t\t\t\t Gradients\n')
+%     fprintf('\t# of solutions in GPS subset: \t\t\t%d\n', sum(S_indicators_grad(:,m)))  
+%     fprintf('\t# of solutions in GPS relaxed subset: \t%d\n\n', sum(S_poly_indicators_grad(:,m)))
+% 
+%     % Gradient Plausible Screening w/ Dgradinf
+%     [S_indicators_gradinf(:,m), D_x0s_gradinf(:,m), S_poly_indicators_gradinf(:,m)] = GPSinf_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_gradinf);
+%     fprintf('\nResults of GPS screening (dinf) \n-------------------------------------------------------\n')
+%     fprintf('\tstandardized discrepancy: \t\t\t\t Gradients\n')
+%     fprintf('\t# of solutions in GPS inf subset: \t\t\t%d\n', sum(S_indicators_gradinf(:,m)))  
+%     fprintf('\t# of solutions in GPS inf relaxed subset: \t%d\n\n', sum(S_poly_indicators_gradinf(:,m)))
+%     
+    % Relaxed Gradient Plausible Screening w/ only gradients
+    [Sonlygrad_d2_indicators(:,m), Sonlygrad_dinf_indicators(:,m)] = RGPS_onlygrad_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_grad, D_cutoff_gradinf);
+    fprintf('\nResults of RGPS screening (only gradients) \n-------------------------------------------------------\n')
+    fprintf('\t# of solutions in RGPS (only gradients) d2 subset: \t\t\t%d\n', sum(Sonlygrad_d2_indicators(:,m)))  
+    fprintf('\t# of solutions in RGPS (only gradients) dinf subset: \t%d\n\n', sum(Sonlygrad_dinf_indicators(:,m)))
 end
 
 %%
@@ -231,6 +239,43 @@ for i = 1:length(switch_on)
 end
 
 legend([h3, h4], 'GPS w/ dgradinf', 'RGPS w/ dgradinf', 'Location', 'southeast')
+
+box on
+hold off
+
+%% Plot single 1-D P(x0 in S) for RGPS w/ only gradients (relaxation of M(x0))
+
+alpha = 0.05;
+%load(['ctsnews_N=',num2str(N),'_K=',num2str(K),'_M=',num2str(M),'_iid_',fn_property,'.mat'],'exp_set');
+
+grey_rgb = (192/256)*ones(1,3);
+dark_grey_rgb = (128/256)*ones(1,3);
+figure
+set(gca, 'FontSize', 14, 'LineWidth', 2)
+
+xlim([0,1])
+ylim([0,1.005])
+xlabel('Solution ($x_0$)','interpreter','latex')
+ylabel('Probability of $x_0$ in Subset','interpreter','latex')
+%title(string_names{1},'interpreter','latex')
+
+hold on
+plot(feas_region, (true_mean - min(true_mean))./(max(true_mean) - min(true_mean)), 'color', dark_grey_rgb, 'LineWidth', 1)
+line([0,1], [1-alpha, 1-alpha], 'Color', 'black', 'LineStyle', ':', 'LineWidth', 1.5)
+C11 = mean(Sonlygrad_d2_indicators,2); %sum(all_S_indicators_d2(:,:,1),2)'/M;
+C12 = mean(Sonlygrad_dinf_indicators,2); %sum(all_S_indicators_d2(:,:,1),2)'/M;
+%scatter(feas_region(:,1), C, 'ko','markerfacecolor','k')
+h4 = plot(feas_region(:,1), C12, 'r-', 'LineWidth', 2);
+h3 = plot(feas_region(:,1), C11, 'b-', 'LineWidth', 2);
+plot(exp_set,zeros(1,size(exp_set,2)),'kx','markerfacecolor','k', 'MarkerSize', 12, 'LineWidth', 1)
+
+for i = 1:length(switch_on)
+    x = [switch_on(i)-.5, switch_off(i)+.5, switch_off(i)+.5, switch_on(i)-.5]/card_feas_region;
+    y = [0, 0, 1, 1]; 
+    p=patch(x,y,'b','LineStyle','none','FaceAlpha',0.2);
+end
+
+legend([h3, h4], 'RGPS w/ d2 (only grad)', 'RGPS w/ dinf (only grad)', 'Location', 'southeast')
 
 box on
 hold off
---
+++ src/RGPS_onlygrad_screen.m
@@ -0,0 +1,85 @@
+function [Sonlygrad_d2_indicators, Sonlygrad_dinf_indicators] = RGPS_onlygrad_screen(feas_region, exp_set, sample_mean, sample_mean_grad, sample_full_cov, n_vec, D_cutoff_dgrad, D_cutoff_dgradinf)
+
+% Gradient Plausible Screening with Dinfinity discrepancy
+% Carry out (+ relaxed) plausible screening w.r.t. optimality and convexity.
+% Return:
+%   - a column-vector of indicators on whether x0 in S^poly.
+
+k = size(exp_set, 1);
+d = size(exp_set, 2);
+card_feas_region = size(feas_region, 1);
+
+Sonlygrad_d2_indicators = zeros(card_feas_region, 1);
+Sonlygrad_dinf_indicators = zeros(card_feas_region, 1);
+
+% RGPS with only gradients: d2 discrepancy
+
+all_soln_conditions = zeros(k, k);
+for i = 1:k
+    for j = 1:k
+        xdiff = exp_set(i,:) - exp_set(j,:); % x_i - x_j
+        first_term = sqrt(D_cutoff_dgrad*(1/n_vec(i)*[1,-xdiff]*sample_full_cov(:,:,i)*[1;-xdiff'] + 1/n_vec(j)*sample_full_cov(1,1,j)));
+        second_term = sample_mean(i) - sample_mean(j) - xdiff*sample_mean_grad(i,:)';
+        all_soln_conditions(i,j) = first_term - second_term;
+    end
+end
+
+if min(min(all_soln_conditions)) >= 0 
+
+    for l = 1:card_feas_region
+        %fprintf('Solution %d.\n', l)
+        x0 = feas_region(l,:);
+        
+        % Relaxed Gradient Plausible Screening w/ only gradients
+        RHS_vec = zeros(k,1);
+        for i = 1:k
+            xdiff = exp_set(i,:) - x0; % x_i - x0
+            first_term = sqrt(D_cutoff_dgrad*(1/n_vec(i)*[0,-xdiff]*sample_full_cov(:,:,i)*[0;-xdiff']));
+            second_term = xdiff*sample_mean_grad(i)';
+            RHS_vec(i) = first_term + second_term;
+        end
+        
+        % Classify solution x0 via relaxation
+        Sonlygrad_d2_indicators(l) = (0 <= min(RHS_vec)); 
+
+    end % end for
+
+% otherwise all solutions are screened out
+end % end if    
+
+% RGPS with only gradients: dinfinity discrepancy
+
+all_soln_conditions = zeros(k, k);
+for i = 1:k
+    for j = 1:k
+        xdiff = exp_set(i,:) - exp_set(j,:); % x_i - x_j
+        first_term = sqrt(D_cutoff_dgradinf*(1/n_vec(i)*[1,-xdiff]*sample_full_cov(:,:,i)*[1;-xdiff']) + sqrt(D_cutoff_dgradinf*1/n_vec(j)*sample_full_cov(1,1,j)));
+        second_term = sample_mean(i) - sample_mean(j) - xdiff*sample_mean_grad(i,:)';
+        all_soln_conditions(i,j) = first_term - second_term;
+    end
+end
+
+if min(min(all_soln_conditions)) >= 0 
+
+    for l = 1:card_feas_region
+        %fprintf('Solution %d.\n', l)
+        x0 = feas_region(l,:);
+                
+        % Relaxed Gradient Plausible Screening w/ only gradients
+        RHS_vec = zeros(k,1);
+        for i = 1:k
+            xdiff = exp_set(i,:) - x0; % x_i - x0
+            first_term = sqrt(D_cutoff_dgradinf*(1/n_vec(i)*[0,-xdiff]*sample_full_cov(:,:,i)*[0;-xdiff']));
+            second_term = xdiff*sample_mean_grad(i)';
+            RHS_vec(i) = first_term + second_term;
+        end
+        
+        % Classify solution x0 via relaxation
+        Sonlygrad_dinf_indicators(l) = (0 <= min(RHS_vec)); 
+
+    end % end for
+
+% otherwise all solutions are screened out
+end % end if    
+
+end
