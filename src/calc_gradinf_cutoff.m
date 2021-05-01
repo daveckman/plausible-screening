1bf8d4b00152b32b7bb7bf7576f6de4638224335
daveckman <david.eckman@northwestern.edu>
2021-05-01 15:48:19 -0500

    update tighter PSG cutoffs for convex case
    Switch to dimension-independent cutoffs for PSG. Reran experiments.

--- src/calc_gradinf_cutoff.m
+++ src/calc_gradinf_cutoff.m
@@ -6,7 +6,11 @@ n_MC_reps = 100000; % Number of Monte Carlo replications for estimation
 
 terms = zeros(k, n_MC_reps);
 for i = 1:k
-    terms(i,:) = (d+1)*(n_vec(i)-1)/(n_vec(i) - (d+1))*frnd(d + 1, n_vec(i) - (d + 1), [1, n_MC_reps]);
+    % Old cutoff
+    %terms(i,:) = (d+1)*(n_vec(i)-1)/(n_vec(i) - (d+1))*frnd(d + 1, n_vec(i) - (d + 1), [1, n_MC_reps]);
+    
+    % New cutoff
+    terms(i,:) = 2*(n_vec(i)-1)/(n_vec(i) - 2)*frnd(2, n_vec(i) - 2, [1, n_MC_reps]);
 end
 D_cutoff = quantile(max(terms, [], 1), 1-alpha);
         
